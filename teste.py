dbdd1d598e0e9594b2b014551ea12d6391ce7f85
ViniciusM15IFC <142498596+ViniciusM15IFC@users.noreply.github.com>
2025-06-18 18:34:23 +0000

    Teste 2

--- teste.py
+++ teste.py
@@ -1,10 +1,15 @@
 import pdfplumber
 
-with pdfplumber.open("Cardápio de mes junho - 2025.pdf") as pdf:
+with pdfplumber.open("cardapio_junho.pdf") as pdf:
     first_page = pdf.pages[0]
     table = first_page.extract_table()
     
     # Para obter como DataFrame do pandas
     import pandas as pd
     df = pd.DataFrame(table[1:], columns=table[0])
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+
     print(df)
+    df.to_csv('tabela.csv', index=False)
+
